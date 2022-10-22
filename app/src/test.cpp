f0c43b1c3acb58cfab1f7aef5dd4bbc424e119bd
Ismael Bueno <ismaelwno@gmail.com>
2022-10-23 00:49:33 +0200

    Benchmarked rk integration

--- app/src/test.cpp
+++ app/src/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "prefab.hpp"
 #include "integrator.hpp"
+#include "timer.hpp"
 
 using namespace vec;
 
@@ -11,6 +12,7 @@ struct particle
 
 rk::state ode(const float t, const particle &p)
 {
+    std::cout << p.m_state.pos << "\n";
     return {p.m_state.vel, -p.m_state.pos};
 }
 
@@ -18,16 +20,21 @@ int main()
 {
     particle p;
     p.m_state = {{1.f, 1.f}, {0.f, 0.f}};
-    rk::integrator integ(rk::rkf78, p.m_state);
+    rk::integrator integ(rk::rk4, p.m_state);
     rk::state st1, st2;
     st1 = st2;
 
     const float dt = 0.01f, tf = 2.0f;
     float t = 0.0f;
-    while (t < tf)
+
+    std::size_t iters = 0;
+    while (t < tf && iters < 2)
     {
+        benchmark::timer tm(std::cout);
         integ.forward(t, dt, p, ode);
         t += dt;
-        std::cout << p.m_state.pos << " error: " << integ.error() << "\n";
+        // std::cout << p.m_state.pos << " error: " << integ.error() << "\n";
+        iters++;
     }
+    std::cout << iters << "\n";
 }
