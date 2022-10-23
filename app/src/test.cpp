b1bed0b8a2730bb2fa7bd998a7b4bf96dff0bc1d
Ismael Bueno <ismaelwno@gmail.com>
2022-10-24 00:10:02 +0200

    Testing std functions to sort and get min and max values from vector

--- app/src/test.cpp
+++ app/src/test.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include "vec2.hpp"
+#include <vector>
+#include <algorithm>
+
+using namespace vec;
+
+bool cmp(const vec2 &v1, const vec2 &v2)
+{
+    return v1.angle() < v2.angle();
+}
 
 int main()
 {
-    std::cout << "Hello test!\n";
-    vec::vec2 v(1.f, 2.f);
+    std::vector<vec2> v = {{1.f, 2.f}, {-1.f, 1.f}, {-1.f, -1.f}};
+    std::sort(v.begin(), v.end(), cmp);
+    for (const vec2 &vc : v)
+        std::cout << vc << "\n";
+
+    const auto &[min, max] = std::minmax_element(v.begin(), v.end(), cmp);
+    std::cout << *min << "\n";
 }
