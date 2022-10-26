be95e89cf3970586d64acccccf6563a3771514dc
Ismael Bueno <ismaelwno@gmail.com>
2022-10-26 22:39:58 +0200

    Tested all the new features

--- app/src/test.cpp
+++ app/src/test.cpp
@@ -2,21 +2,17 @@
 #include "vec2.hpp"
 #include <vector>
 #include <algorithm>
+#include "box2D.hpp"
 #include "polygon2D.hpp"
 
 using namespace geo;
 
 int main()
 {
-    const polygon2D p1({{1.f, 0.f}, {0.f, 1.f}, {0.f, -1.f}});
-    const polygon2D p2({{0.f, 0.f}, {1.f, 1.f}, {1.f, -1.f}});
-    const polygon2D dp = p1 - p2;
+    const polygon2D p1({{-4.f, 2.f}, {-3.f, 1.f}, {-2.f, 2.f}});
+    const polygon2D p2({{2.f, 1.f}, {2.f, 3.f}, {4.f, 1.f}, {4.f, 3.f}});
+    const polygon2D dp = p1 + p2;
 
-    // for (const vec2 &v : df.vertices())
-    //     std::cout << v << "\n";
-    const vec2 dir = {-1.f, .5f};
-    std::cout << p1.support_vertex(dir) << "\n";
-    std::cout << p2.support_vertex(dir) << "\n";
-    std::cout << dp.support_vertex(dir) << "\n";
-    std::cout << p1.support_vertex(dir) - p2.support_vertex(-dir) << "\n";
+    // for (const vec2 &v : dp.vertices())
+    std::cout << dp.centre_of_vertices() << "\n";
 }
