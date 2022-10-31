5b0630f34c5cea938c96a7ea249a6c1f67043f42
Ismael Bueno <ismaelwno@gmail.com>
2022-10-31 23:41:12 +0100

    Implemented const vector of vec_ptr

--- vec-ptr/include/vec_ptr.hpp
+++ vec-ptr/include/vec_ptr.hpp
@@ -6,16 +6,31 @@
 
 namespace utils
 {
+    class const_vec_ptr
+    {
+    public:
+        const_vec_ptr() = default;
+        const_vec_ptr(const std::vector<float> &buffer, std::size_t index = 0);
+
+        const float &operator[](const std::size_t idx) const;
+
+        explicit operator bool() const;
+
+    private:
+        const std::vector<float> *m_buffer = nullptr;
+        std::size_t m_index = 0;
+    };
+
     class vec_ptr
     {
     public:
         vec_ptr() = default;
         vec_ptr(std::vector<float> &buffer, std::size_t index = 0);
 
-        float &operator[](const std::size_t idx);
-        const float &operator[](const std::size_t idx) const;
+        float &operator[](const std::size_t idx) const;
 
         explicit operator bool() const;
+        operator const_vec_ptr() const;
 
     private:
         std::vector<float> *m_buffer = nullptr;
--- vec-ptr/src/vec_ptr.cpp
+++ vec-ptr/src/vec_ptr.cpp
@@ -2,10 +2,14 @@
 
 namespace utils
 {
+    const_vec_ptr::const_vec_ptr(const std::vector<float> &buffer, std::size_t index) : m_buffer(&buffer), m_index(index) {}
+
+    const float &const_vec_ptr::operator[](const std::size_t idx) const { return m_buffer->operator[](m_index + idx); }
+    const_vec_ptr::operator bool() const { return m_buffer; }
+
     vec_ptr::vec_ptr(std::vector<float> &buffer, std::size_t index) : m_buffer(&buffer), m_index(index) {}
 
-    float &vec_ptr::operator[](const std::size_t idx) { return m_buffer->operator[](m_index + idx); }
-    const float &vec_ptr::operator[](const std::size_t idx) const { return m_buffer->operator[](m_index + idx); }
-
+    float &vec_ptr::operator[](const std::size_t idx) const { return m_buffer->operator[](m_index + idx); }
     vec_ptr::operator bool() const { return m_buffer; }
+    vec_ptr::operator const_vec_ptr() const { return const_vec_ptr(*m_buffer, m_index); }
 }
