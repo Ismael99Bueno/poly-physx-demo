5ab1b3672bd6002c9a1c5360a36d09cf75b195f0
Ismael Bueno <ismaelwno@gmail.com>
2022-10-31 23:36:20 +0100

    Implemented const versions of the entity pointer

--- engine/include/entity_ptr.hpp
+++ engine/include/entity_ptr.hpp
@@ -6,18 +6,34 @@
 
 namespace physics
 {
+    class const_entity_ptr
+    {
+    public:
+        const_entity_ptr(const std::vector<entity2D> &vec, std::size_t index = 0);
+
+        const entity2D *operator->() const;
+        const entity2D &operator*() const;
+
+        explicit operator bool() const;
+
+    private:
+        const std::vector<entity2D> *m_vec = nullptr;
+        std::size_t m_index = 0;
+    };
+
+    bool operator==(const const_entity_ptr &e1, const const_entity_ptr &e2);
+    bool operator!=(const const_entity_ptr &e1, const const_entity_ptr &e2);
+
     class entity_ptr
     {
     public:
         entity_ptr(std::vector<entity2D> &vec, std::size_t index = 0);
 
-        const entity2D *operator->() const;
-        const entity2D &operator*() const;
-
-        entity2D *operator->();
-        entity2D &operator*();
+        entity2D *operator->() const;
+        entity2D &operator*() const;
 
         explicit operator bool() const;
+        operator const_entity_ptr() const;
 
     private:
         std::vector<entity2D> *m_vec = nullptr;
--- engine/src/entity_ptr.cpp
+++ engine/src/entity_ptr.cpp
@@ -2,16 +2,25 @@
 
 namespace physics
 {
+    const_entity_ptr::const_entity_ptr(const std::vector<entity2D> &vec,
+                                       const std::size_t index) : m_vec(&vec), m_index(index) {}
+
+    const entity2D *const_entity_ptr::operator->() const { return &((*m_vec)[m_index]); }
+    const entity2D &const_entity_ptr::operator*() const { return (*m_vec)[m_index]; }
+
+    const_entity_ptr::operator bool() const { return m_vec; }
+
+    bool operator==(const const_entity_ptr &e1, const const_entity_ptr &e2) { return &(*e1) == &(*e2); }
+    bool operator!=(const const_entity_ptr &e1, const const_entity_ptr &e2) { return !(e1 == e2); }
+
     entity_ptr::entity_ptr(std::vector<entity2D> &vec,
                            const std::size_t index) : m_vec(&vec), m_index(index) {}
 
-    const entity2D *entity_ptr::operator->() const { return &((*m_vec)[m_index]); }
-    const entity2D &entity_ptr::operator*() const { return (*m_vec)[m_index]; }
+    entity2D *entity_ptr::operator->() const { return &((*m_vec)[m_index]); }
+    entity2D &entity_ptr::operator*() const { return (*m_vec)[m_index]; }
 
-    entity2D *entity_ptr::operator->() { return &((*m_vec)[m_index]); }
-    entity2D &entity_ptr::operator*() { return (*m_vec)[m_index]; }
-
-    explicit entity_ptr::operator bool() const { return m_vec; }
+    entity_ptr::operator bool() const { return m_vec; }
+    entity_ptr::operator const_entity_ptr() const { return const_entity_ptr(*m_vec, m_index); }
 
     bool operator==(const entity_ptr &e1, const entity_ptr &e2) { return &(*e1) == &(*e2); }
     bool operator!=(const entity_ptr &e1, const entity_ptr &e2) { return !(e1 == e2); }
