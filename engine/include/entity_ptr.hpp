dbce14b99122c958ec83b455dd3cfa0f9088d9a5
Ismael Bueno <ismaelwno@gmail.com>
2022-10-31 22:56:33 +0100

    Now entity_ptr takes a pointer to a vector

--- engine/include/entity_ptr.hpp
+++ engine/include/entity_ptr.hpp
@@ -9,15 +9,19 @@ namespace physics
     class entity_ptr
     {
     public:
-        entity_ptr() = delete;
-        entity_ptr(const std::vector<entity2D> &vec, std::size_t index);
+        entity_ptr(std::vector<entity2D> &vec, std::size_t index = 0);
 
         const entity2D *operator->() const;
         const entity2D &operator*() const;
 
+        entity2D *operator->();
+        entity2D &operator*();
+
+        explicit operator bool() const;
+
     private:
-        const std::vector<entity2D> &m_vec;
-        std::size_t m_index;
+        std::vector<entity2D> *m_vec = nullptr;
+        std::size_t m_index = 0;
     };
 
     bool operator==(const entity_ptr &e1, const entity_ptr &e2);
--- engine/src/entity_ptr.cpp
+++ engine/src/entity_ptr.cpp
@@ -2,11 +2,17 @@
 
 namespace physics
 {
-    entity_ptr::entity_ptr(const std::vector<entity2D> &vec,
-                           const std::size_t index) : m_vec(vec), m_index(index) {}
+    entity_ptr::entity_ptr(std::vector<entity2D> &vec,
+                           const std::size_t index) : m_vec(&vec), m_index(index) {}
 
-    const entity2D *entity_ptr::operator->() const { return &m_vec[m_index]; }
-    const entity2D &entity_ptr::operator*() const { return m_vec[m_index]; }
+    const entity2D *entity_ptr::operator->() const { return &((*m_vec)[m_index]); }
+    const entity2D &entity_ptr::operator*() const { return (*m_vec)[m_index]; }
+
+    entity2D *entity_ptr::operator->() { return &((*m_vec)[m_index]); }
+    entity2D &entity_ptr::operator*() { return (*m_vec)[m_index]; }
+
+    explicit entity_ptr::operator bool() const { return m_vec; }
+
     bool operator==(const entity_ptr &e1, const entity_ptr &e2) { return &(*e1) == &(*e2); }
     bool operator!=(const entity_ptr &e1, const entity_ptr &e2) { return !(e1 == e2); }
 }
