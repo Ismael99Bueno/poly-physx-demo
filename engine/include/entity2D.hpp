0c127ebee77806bb1aeca67fdde1b40eb104b572
Ismael Bueno <ismaelwno@gmail.com>
2022-10-31 22:15:11 +0100

    Implemented buffer

--- engine/include/entity2D.hpp
+++ engine/include/entity2D.hpp
@@ -5,6 +5,7 @@
 #include "integrator.hpp"
 #include "box2D.hpp"
 #include "polygon2D.hpp"
+#include "vec_ptr.hpp"
 
 namespace physics
 {
@@ -13,12 +14,16 @@ namespace physics
     public:
         using body2D::body2D;
 
+        void retrieve(const utils::vec_ptr &buffer);
+        void retrieve();
+
         const geo::box2D &bounding_box() const;
         const geo::polygon2D &shape() const;
 
     private:
         geo::box2D m_bbox;
         geo::polygon2D m_shape;
+        utils::vec_ptr m_buffer;
     };
 }
 
--- engine/src/entity2D.cpp
+++ engine/src/entity2D.cpp
@@ -2,6 +2,17 @@
 
 namespace physics
 {
+    void entity2D::retrieve(const utils::vec_ptr &buffer)
+    {
+        DBG_EXIT_IF(!buffer, "Cannot retrieve from a null buffer.\n")
+        m_pos = {buffer[0], buffer[1]};
+        m_vel = {buffer[3], buffer[4]};
+        m_angpos = buffer[2];
+        m_angvel = buffer[5];
+    }
+
+    void entity2D::retrieve() { retrieve(m_buffer); }
+
     const geo::box2D &entity2D::bounding_box() const { return m_bbox; }
     const geo::polygon2D &entity2D::shape() const { return m_shape; }
 }
