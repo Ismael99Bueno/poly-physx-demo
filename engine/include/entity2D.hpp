f0a4fc027025731c5d25998a20e553e847842ca0
Ismael Bueno <ismaelwno@gmail.com>
2022-10-31 21:28:49 +0100

    Now entity inherits from body

--- engine/include/entity2D.hpp
+++ engine/include/entity2D.hpp
@@ -1,22 +1,22 @@
 #ifndef ENTITY2D_HPP
 #define ENTITY2D_HPP
 
-#include "state.hpp"
+#include "body2D.hpp"
 #include "integrator.hpp"
 #include "box2D.hpp"
 #include "polygon2D.hpp"
 
 namespace physics
 {
-    class entity2D
+    class entity2D : public body2D
     {
     public:
+        using body2D::body2D;
+
         const geo::box2D &bounding_box() const;
         const geo::polygon2D &shape() const;
 
     private:
-        rk::state m_state;
-        rk::integrator m_integ;
         geo::box2D m_bbox;
         geo::polygon2D m_shape;
     };
