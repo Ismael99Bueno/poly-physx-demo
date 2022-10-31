123d5095101f0cda1595a316384af0d0a437cedf
Ismael Bueno <ismaelwno@gmail.com>
2022-10-31 21:16:15 +0100

    Tested rk and its new functionality

--- app/src/test.cpp
+++ app/src/test.cpp
@@ -2,32 +2,36 @@
 #include "prefab.hpp"
 #include "integrator.hpp"
 
-using namespace vec;
-
 struct particle
 {
-    rk::state m_state;
+    std::vector<float> state;
 };
 
-rk::state ode(const float t, const particle &p)
+std::vector<float> ode(const float t, const std::vector<float> &state, const particle &p)
 {
-    return {p.m_state.vel, -p.m_state.pos, {p.m_state.angular.y, -p.m_state.angular.x}};
+    std::vector<float> res(p.state.size());
+    res[0] = state[3];
+    res[1] = state[4];
+    res[2] = state[5];
+    res[3] = -state[0];
+    res[4] = -state[1];
+    res[5] = -state[2];
+
+    return res;
 }
 
 int main()
 {
-    particle p;
-    p.m_state = {{1.f, 1.f}, {0.f, 0.f}, {1.0f, 0.0f}};
-    rk::integrator integ(rk::rkf78, p.m_state);
-    rk::state st1, st2;
-    st1 = st2;
+    particle p = {{1.f, 1.f, 0.f, 0.f, 1.0f, 0.0f}};
+    rk::integrator integ(rk::rkf78, p.state);
 
-    const float dt = 0.01f, tf = 2.0f;
+    float dt = 0.1f, tf = 20.0f;
     float t = 0.0f;
-    while (t < tf)
+    while (t + dt < tf)
     {
-        integ.forward(t, dt, p, ode);
-        t += dt;
-        std::cout << p.m_state.pos << " error: " << integ.error() << "\n";
+        integ.raw_forward(t, dt, p, ode);
+        std::cout << "t: " << t << " dt: " << dt << " x: " << p.state[0] << " error: " << integ.error() << "\n";
     }
+    integ.raw_forward(t, tf - t, p, ode);
+    std::cout << "t: " << t << " dt: " << dt << " x: " << p.state[0] << " error: " << integ.error() << "\n";
 }
