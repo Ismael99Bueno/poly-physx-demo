673ef6474e1e6a78a31a456d8ae781ad1c7c228c
Ismael Bueno <ismaelwno@gmail.com>
2022-11-03 00:31:17 +0100

    Switched back to passing a reference of an entity

--- engine/include/interaction2D.hpp
+++ engine/include/interaction2D.hpp
@@ -10,7 +10,7 @@ namespace physics
     {
     public:
         interaction2D(std::size_t allocations = 50);
-        virtual std::pair<vec2, float> acceleration(const body2D &b1, const body2D &b2) const = 0;
+        virtual std::pair<vec2, float> acceleration(const entity2D &e1, const entity2D &e2) const = 0;
 
         void add(const entity_ptr &e);
         void remove(const entity_ptr &e);
