e8f64233efc026839d92cc2e108e1105587c30d1
Ismael Bueno <ismaelwno@gmail.com>
2022-11-03 18:34:30 +0100

    Now ode calls the accel method on entity

--- engine/src/ode2D.cpp
+++ engine/src/ode2D.cpp
@@ -8,16 +8,16 @@ namespace physics
         result.reserve(state.size());
 
         engine.retrieve(state);
+        engine.reset_accelerations(); // REWORK HOW ENTITIES SAVE FORCES!!
         for (std::size_t i = 0; i < engine.m_entities.size(); i++)
         {
-            entity2D &e = engine.m_entities[i];
-            const std::pair<vec2, float> &accel = e.compute_accel();
+            const entity2D &e = engine.m_entities[i];
+            const std::pair<vec2, float> &accel = e.accel();
 
             const std::size_t j = VAR_PER_ENTITY * i;
             result.insert(result.end(), {state[j + 3], state[j + 4], state[j + 5],
                                          accel.first.x, accel.first.y, accel.second});
         }
-        engine.reset_accelerations();
         return result;
     }
 }
