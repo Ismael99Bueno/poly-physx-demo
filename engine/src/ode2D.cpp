027c9dd70a937c802ff0171d9f6604b91078409e
Ismael Bueno <ismaelwno@gmail.com>
2022-11-05 23:13:16 +0100

    Applied constrains to ode

--- engine/src/ode2D.cpp
+++ engine/src/ode2D.cpp
@@ -4,8 +4,8 @@ namespace physics
 {
     std::vector<float> ode(float t, const std::vector<float> &state, engine2D &engine)
     {
-        std::vector<float> result;
-        result.reserve(state.size());
+        std::vector<float> stchanges;
+        stchanges.reserve(state.size());
 
         engine.retrieve(state);
         engine.reset_accelerations(); // REWORK HOW ENTITIES SAVE FORCES!!
@@ -15,13 +15,13 @@ namespace physics
             const std::pair<vec2, float> &accel = e.accel();
 
             const std::size_t j = VAR_PER_ENTITY * i;
-            result.insert(result.end(), {state[j + 3], state[j + 4], state[j + 5],
-                                         accel.first.x, accel.first.y, accel.second});
+            stchanges.insert(stchanges.end(), {state[j + 3], state[j + 4], state[j + 5],
+                                               accel.first.x, accel.first.y, accel.second});
         }
-        const std::vector<float> accels = engine.m_compeller.solve_constrains(state, result);
+        const std::vector<float> accels = engine.m_compeller.solve_constrains(stchanges);
         for (std::size_t i = 0; i < engine.m_entities.size(); i++)
             for (std::size_t j = 0; j < 3; j++)
-                result[6 * i + j + 3] += accels[3 * i + j];
-        return result;
+                stchanges[6 * i + j + 3] += accels[3 * i + j];
+        return stchanges;
     }
 }
