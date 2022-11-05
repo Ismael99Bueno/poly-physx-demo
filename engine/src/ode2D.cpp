1aaeadfaf28596ea8bc221847339c976c35bfc55
Ismael Bueno <ismaelwno@gmail.com>
2022-11-05 12:14:19 +0100

    Solving constrains in the ode

--- engine/src/ode2D.cpp
+++ engine/src/ode2D.cpp
@@ -18,6 +18,10 @@ namespace physics
             result.insert(result.end(), {state[j + 3], state[j + 4], state[j + 5],
                                          accel.first.x, accel.first.y, accel.second});
         }
+        const std::vector<float> accels = engine.m_compeller.solve_constrains(state, result);
+        for (std::size_t i = 0; i < engine.m_entities.size(); i++)
+            for (std::size_t j = 0; j < 3; j++)
+                result[6 * i + j + 3] += accels[3 * i + j];
         return result;
     }
 }
