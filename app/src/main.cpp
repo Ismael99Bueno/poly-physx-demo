d9a3ebce77a45a18e327b5cb4e2b738a5bb9a7fd
Ismael Bueno <ismaelwno@gmail.com>
2022-11-06 19:52:22 +0100

    Tested environment

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "prefab.hpp"
 #include "interaction2D.hpp"
-#include "engine2D.hpp"
+#include "environment.hpp"
 #include "timer.hpp"
 #include "constrain2D.hpp"
 
@@ -48,56 +48,6 @@ void set_points(sf::ConvexShape &cshape, sf::RectangleShape &rshape, const const
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!");
-    window.setView(sf::View(sf::Vector2f(0.f, 0.f), sf::Vector2f(WIDTH, -HEIGHT)));
-
-    engine2D eng(rk::rkf78);
-    eng.integrator().tolerance(1.e-8);
-    entity_ptr e1 = eng.add_entity({100.f, 0.f}, {-30.f, 1.f}, M_PI / 3.f), e2 = eng.add_entity(); //, e3 = eng.add({-100.f, -100.f});
-
-    const geo::polygon2D &s1 = e1->shape(geo::polygon2D(e1->pos(), {{-30.f, 0.f}, {30.f, 0.f}, {0.f, 30.f}, {00.f, -30.f}})),
-                         &s2 = e2->shape(geo::polygon2D(e2->pos(), {{-30.f, 0.f}, {30.f, 0.f}, {0.f, 30.f}}));
-
-    // gravitation grav;
-    // grav.add(e1);
-    // grav.add(e2);
-    // grav.add(e3);
-
-    // stick st;
-    // st.add_entities({e1, e2});
-    // eng.add_constrain(st);
-
-    sf::ConvexShape c1, c2;
-    sf::RectangleShape r1, r2;
-
-    set_points(c1, r1, e1);
-    set_points(c2, r2, e2);
-
-    c1.setFillColor(sf::Color::Green);
-    c2.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        benchmark::timer tm(std::cout);
-        window.clear();
-        eng.raw_forward();
-        e1->retrieve();
-        e2->retrieve();
-        set_points(c1, r1, e1);
-        set_points(c2, r2, e2);
-        // window.draw(r1);
-        // window.draw(r2);
-        window.draw(c1);
-        window.draw(c2);
-        window.display();
-    }
-
-    return 0;
+    app::environment env(rk::rk4);
+    env.run();
 }
