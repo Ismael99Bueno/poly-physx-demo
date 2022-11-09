98a2244a45ba25c904cee6e12ea8fcfba57e7561
Ismael Bueno <ismaelwno@gmail.com>
2022-11-09 19:01:37 +0100

    Reworked collision constrain with new separation points

--- engine/include/collider2D.hpp
+++ engine/include/collider2D.hpp
@@ -12,21 +12,27 @@ namespace physics
     class collider2D
     {
     public:
-        collider2D() = delete;
-        collider2D(const std::vector<entity2D> &entities, std::size_t allocations = 40);
-
-        void add(std::size_t index); // TODO: Implement remove
-        void detect_collisions();
-
-    private:
-        struct collision_pair
+        class collision_constrain : public constrain2D<2>
         {
         public:
-            collision_pair() = delete;
-            collision_pair(const const_entity_ptr &e1, const const_entity_ptr &e2);
-            const_entity_ptr e1, e2;
+            using constrain2D<2>::constrain2D;
+
+        private:
+            vec2 m_touch1, m_touch2;
+            float constrain(const std::array<const_entity_ptr, 2> &entities) const override;
+            float constrain_derivative(const std::array<const_entity_ptr, 2> &entities) const override;
+
+            void compute_touch_points();
+            std::pair<vec2, vec2> compute_touch_velocities() const;
         };
 
+        collider2D() = delete;
+        collider2D(std::vector<entity2D> &entities, std::size_t allocations = 40);
+
+        void add(std::size_t index); // TODO: Implement remove
+        const std::vector<collision_constrain> &build_collision_constrains();
+
+    private:
         struct interval
         {
         public:
@@ -36,35 +42,21 @@ namespace physics
                 HIGHER
             };
 
-            interval(const const_entity_ptr &e, end end_type);
+            interval(const entity_ptr &e, end end_type);
 
-            const const_entity_ptr &entity() const;
+            const entity_ptr &entity() const;
             float value() const;
             end type() const;
 
         private:
-            const_entity_ptr m_entity;
+            entity_ptr m_entity;
             end m_end;
         };
 
-        class collision_constrain : public constrain2D<2>
-        {
-        public:
-            using constrain2D<2>::constrain2D;
-
-        private:
-            const vec2 *m_vx1, *m_vx2;
-            float constrain(const std::array<const_entity_ptr, 2> &entities) const override;
-            float constrain_derivative(const std::array<const_entity_ptr, 2> &entities) const override;
-
-            void compute_closest_vertices();
-            std::pair<vec2, vec2> compute_vertices_velocities() const;
-        };
-
-        const std::vector<entity2D> &m_buffer;
-        std::vector<const_entity_ptr> m_entities;
+        std::vector<entity2D> &m_buffer;
+        std::vector<entity_ptr> m_entities;
         std::vector<interval> m_intervals;
-        std::vector<collision_pair> m_collisions;
+        std::vector<collision_constrain> m_constrains;
 
         void sort_entities();
     };
--- engine/src/collider2D.cpp
+++ engine/src/collider2D.cpp
@@ -5,20 +5,17 @@
 
 namespace physics
 {
-    collider2D::collider2D(const std::vector<entity2D> &entities,
+    collider2D::collider2D(std::vector<entity2D> &entities,
                            const std::size_t allocations) : m_buffer(entities)
     {
         m_entities.reserve(allocations);
         m_intervals.reserve(allocations);
-        m_collisions.reserve(allocations);
+        m_constrains.reserve(allocations);
     }
 
-    collider2D::collision_pair::collision_pair(const const_entity_ptr &e1,
-                                               const const_entity_ptr &e2) : e1(e1), e2(e2) {}
+    collider2D::interval::interval(const entity_ptr &e, const end end_type) : m_entity(e), m_end(end_type) {}
 
-    collider2D::interval::interval(const const_entity_ptr &e, const end end_type) : m_entity(e), m_end(end_type) {}
-
-    const const_entity_ptr &collider2D::interval::entity() const { return m_entity; }
+    const entity_ptr &collider2D::interval::entity() const { return m_entity; }
 
     float collider2D::interval::value() const
     {
@@ -27,28 +24,21 @@ namespace physics
 
     collider2D::interval::end collider2D::interval::type() const { return m_end; }
 
-    void collider2D::collision_constrain::compute_closest_vertices()
+    void collider2D::collision_constrain::compute_touch_points()
     {
-        float min_dist = std::numeric_limits<float>::max();
         const const_entity_ptr &e1 = m_entities[0], &e2 = m_entities[1];
-        for (const vec2 &v1 : e1->shape().vertices())
-            for (const vec2 &v2 : e2->shape().vertices())
-            {
-                const float dist = v1.sq_dist(v2);
-                if (dist < min_dist)
-                {
-                    min_dist = dist;
-                    m_vx1 = &v1;
-                    m_vx2 = &v2;
-                }
-            }
+        const auto [sep11, sep12] = e1->shape().separation_points(e2->shape());
+        const auto [sep21, sep22] = e2->shape().separation_points(e1->shape());
+        const float d1 = sep11.sq_dist(sep12), d2 = sep21.sq_dist(sep22);
+        m_touch1 = d1 < d2 ? sep11 : sep21;
+        m_touch2 = d1 < d2 ? sep12 : sep22;
     }
 
-    std::pair<vec2, vec2> collider2D::collision_constrain::compute_vertices_velocities() const
+    std::pair<vec2, vec2> collider2D::collision_constrain::compute_touch_velocities() const
     {
         const const_entity_ptr &e1 = m_entities[0], &e2 = m_entities[1];
-        const vec2 rel_vx1 = (*m_vx1 - e1->shape().centroid()),
-                   rel_vx2 = (*m_vx2 - e2->shape().centroid());
+        const vec2 rel_vx1 = (m_touch1 - e1->shape().centroid()),
+                   rel_vx2 = (m_touch2 - e2->shape().centroid());
         const float a1 = rel_vx2.angle(), a2 = rel_vx2.angle();
         return {e1->vel() + rel_vx1.norm() * e1->angvel() * vec2(-std::sin(a1), std::cos(a1)),
                 e2->vel() + rel_vx2.norm() * e2->angvel() * vec2(-std::sin(a2), std::cos(a2))};
@@ -56,42 +46,43 @@ namespace physics
 
     float collider2D::collision_constrain::constrain(const std::array<const_entity_ptr, 2> &entities) const
     {
-        return m_vx1->sq_dist(*m_vx2);
+        return m_touch1.sq_dist(m_touch2);
     }
 
     float collider2D::collision_constrain::constrain_derivative(const std::array<const_entity_ptr, 2> &entities) const
     {
-        const auto [v1, v2] = compute_vertices_velocities();
-        return 2.f * (*m_vx1 - *m_vx2).dot(v1 - v2);
+        const auto [v1, v2] = compute_touch_velocities();
+        return 2.f * (m_touch1 - m_touch2).dot(v1 - v2);
     }
 
     void collider2D::add(const std::size_t index)
     {
         m_entities.emplace_back(m_buffer, index);
-        const const_entity_ptr &e = m_entities[m_entities.size() - 1];
+        const entity_ptr &e = m_entities[m_entities.size() - 1];
         m_intervals.emplace_back(e, interval::LOWER);
         m_intervals.emplace_back(e, interval::HIGHER);
     }
 
-    void collider2D::detect_collisions()
+    const std::vector<collider2D::collision_constrain> &collider2D::build_collision_constrains()
     {
         sort_entities();
-        m_collisions.clear();
+        m_constrains.clear();
 
-        std::vector<const_entity_ptr> eligible;
+        std::vector<entity_ptr> eligible;
         eligible.reserve(6);
         for (const interval &itrv : m_intervals)
             if (itrv.type() == interval::LOWER)
                 eligible.emplace_back(itrv.entity());
             else
             {
-                for (const const_entity_ptr &entity : eligible)
+                for (const entity_ptr &entity : eligible)
                     if (entity != itrv.entity() &&
                         entity->bounding_box().overlaps(itrv.entity()->bounding_box()) &&
                         entity->shape().overlaps(itrv.entity()->shape()))
-                        m_collisions.emplace_back(entity, itrv.entity());
+                        m_constrains.emplace_back(std::array<entity_ptr, 2>({entity, itrv.entity()}));
                 eligible.clear();
             }
+        return m_constrains;
     }
 
     void collider2D::sort_entities()
