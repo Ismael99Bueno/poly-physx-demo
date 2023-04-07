2880428b1663e9a2e182f134f1be563529d4c610
Ismael Bueno <ismaelwno@gmail.com>
2023-04-07 23:15:35 +0100

    Exceptions now actually display their message

--- scripts/exceptions.py
+++ scripts/exceptions.py
@@ -13,6 +13,7 @@ class UnrecognizedWhichArgumentError(Exception):
 class PackageNotFoundError(Exception):
     def __init__(self, package: str) -> None:
         self.__package = package
+        super().__init__(self.__repr__())
 
     def __repr__(self) -> str:
         return f"Package {self.__package} not found"
@@ -22,6 +23,7 @@ class PythonVersionError(Exception):
     def __init__(self, required: str, current: str) -> None:
         self.__required = required
         self.__current = current
+        super().__init__(self.__repr__())
 
     def __repr__(self) -> str:
         return f"Minimum python version required is {self.__required}, but {self.__current} found. Consider upgrading to {self.__required}"
