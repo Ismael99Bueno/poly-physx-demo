1868aba2f0dd3556a7436acad8bd8be5e8aaca51
Ismael Bueno <ismaelwno@gmail.com>
2023-04-08 02:55:46 +0100

    Setup windows calls premake validation

--- scripts/setup_win.py
+++ scripts/setup_win.py
@@ -1,8 +1,11 @@
-from setup_py import validate_python_version
+from setup_py import validate_python_version, validate_python_package
+from setup_win_premake import validate_premake
 
 
 def main() -> None:
     validate_python_version()
+    validate_python_package("requests")
+    validate_premake()
 
 
 if __name__ == "__main__":
