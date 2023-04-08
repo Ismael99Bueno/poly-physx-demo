21715258c362029d3e59f968d5ba6042a1d8caa2
Ismael Bueno <ismaelwno@gmail.com>
2023-04-08 01:40:23 +0100

    Removed SFMLException and replaced it for a more global exception: PathNotFound exception. Now UnrecognizableWhich has an automatic message depending on the unrecognized argument

--- scripts/exceptions.py
+++ scripts/exceptions.py
@@ -1,13 +1,19 @@
-class RootPathNotFoundError(Exception):
-    pass
+class PathNotFoundError(Exception):
+    def __init__(self, path: str) -> None:
+        self.__path = path
+        super().__init__(self.__repr__())
 
-
-class SFMLPathNotFoundError(Exception):
-    pass
+    def __repr__(self) -> str:
+        return f"{self.__path} not found. The project's folder structure may have been changed, or the repository may have not been cloned recursively"
 
 
 class UnrecognizedWhichArgumentError(Exception):
-    pass
+    def __init__(self, arg: str) -> None:
+        self.__arg = arg
+        super().__init__(self.__repr__())
+
+    def __repr__(self) -> str:
+        return f"Unrecognized --which argument '{self.__arg}'. Expected 'all', 'ppx' or 'sfml'."
 
 
 class PackageNotFoundError(Exception):
