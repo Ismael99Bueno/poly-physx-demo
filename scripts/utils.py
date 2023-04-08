136d1895ded55b49a81a6d14672c99e31795387e
Ismael Bueno <ismaelwno@gmail.com>
2023-04-08 02:40:56 +0100

    Implemented functions to download and unzip files

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,3 +5,25 @@ from exceptions import PathNotFoundError
 ROOT_PATH = Path(os.path.dirname(os.path.realpath(__file__))).parent.absolute()
 if not os.path.exists(ROOT_PATH):
     raise PathNotFoundError(ROOT_PATH)
+
+
+def download_file(url: str, path: str) -> None:
+    import requests
+
+    os.makedirs(path, exist_ok=True)
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers, stream=True)
+    with open(path, "wb") as f:
+        f.write(response.content)
+
+
+def unzip_file(path: str) -> None:
+    from zipfile import ZipFile
+
+    with ZipFile(path, "r") as zip:
+        zip.extractall()
+
+    os.remove(path)
