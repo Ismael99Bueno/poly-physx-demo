59bec9f2df78aafcd30a882f5070bb6374f2a4c2
Ismael Bueno <ismaelwno@gmail.com>
2023-04-08 01:38:41 +0100

    Now utils checks if root path exists

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,7 @@
 import os
 from pathlib import Path
+from exceptions import PathNotFoundError
 
 ROOT_PATH = Path(os.path.dirname(os.path.realpath(__file__))).parent.absolute()
+if not os.path.exists(ROOT_PATH):
+    raise PathNotFoundError(ROOT_PATH)
