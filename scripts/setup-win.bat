6eec41e3b25b138d1e58f8e2828e05ea58bfc834
Ismael Bueno <ismaelwno@gmail.com>
2023-04-09 00:07:42 +0100

    Windows setup can be ran with a .bat file

---
+++ scripts/setup-win.bat
@@ -0,0 +1,3 @@
+@echo off
+python setup_win.py
+PAUSE
--- scripts/setup_win.py
+++ scripts/setup_win.py
@@ -1,5 +1,6 @@
 from setup_py import validate_python_version, validate_python_package
 from setup_win_premake import validate_premake
+from setup_win_cmake import validate_cmake
 from exceptions import BadOS
 import platform
 
@@ -10,7 +11,13 @@ def main() -> None:
 
     validate_python_version()
     validate_python_package("requests")
+
     validate_premake()
+    validate_cmake()
+
+    print(
+        "Setup completed successfully! Make sure to re-run the script in case it was necessary to run an external installer"
+    )
 
 
 if __name__ == "__main__":
