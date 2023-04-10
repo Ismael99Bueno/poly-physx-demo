698ba4668f952aee16acb5a066bbad4f661c2388
Ismael Bueno <ismaelwno@gmail.com>
2023-04-10 19:26:56 +0100

    Enabled all sorts of warnings

--- poly-physx-demo/premake5.lua
+++ poly-physx-demo/premake5.lua
@@ -4,7 +4,7 @@ project "poly-physx-demo"
 
    language "C++"
    cppdialect "C++17"
-   buildoptions "-Wall"
+   buildoptions {"-Wall", "-Wextra", "-Wpedantic", "-Wno-unused-parameter"}
    
 
    targetdir("bin/" .. outputdir)
