7ab7c9b8eba71a1b6afb371b6f53a0c696e2434f
Ismael Bueno <ismaelwno@gmail.com>
2023-04-30 23:58:40 +0200

    Formatted premake5 lua files

--- poly-physx-demo/premake5.lua
+++ poly-physx-demo/premake5.lua
@@ -1,54 +1,127 @@
 project "poly-physx-demo"
-   kind "ConsoleApp"
-   staticruntime "off"
-   defines {'ROOT_PATH="' .. rootpath .. '"'}
+kind "ConsoleApp"
+staticruntime "off"
+defines {'ROOT_PATH="' .. rootpath .. '"'}
 
-   language "C++"
-   cppdialect "C++17"
-   filter "system:macosx"
-      buildoptions {"-Wall", "-Wextra", "-Wpedantic", "-Wconversion", "-Wno-unused-parameter"}
-   filter "system:windows"
-      defines "SFML_STATIC"
-   filter{}
+language "C++"
+cppdialect "C++17"
+filter "system:macosx"
+buildoptions {
+   "-Wall",
+   "-Wextra",
+   "-Wpedantic",
+   "-Wconversion",
+   "-Wno-unused-parameter"
+}
+filter "system:windows"
+defines "SFML_STATIC"
+filter {}
 
-   pchheader "pch.hpp"
-   pchsource "src/pch.cpp"
+pchheader "pch.hpp"
+pchsource "src/pch.cpp"
 
-   targetdir("bin/" .. outputdir)
-   objdir("build/" .. outputdir)
+targetdir("bin/" .. outputdir)
+objdir("build/" .. outputdir)
 
-   files {"src/**.cpp", "include/**.hpp"}
-   includedirs {"../**/include", "../vendor/glm"}
+files {
+   "src/**.cpp",
+   "include/**.hpp"
+}
 
-   links {"profile-tools", "ini-parser", "rk-integrator", "shapes-2D", "poly-physx", "imgui", "imgui-SFML", "implot", "poly-physx-app", "sfml-primitives"}
+includedirs {
+   "../**/include",
+   "../vendor/glm"
+}
 
-   filter "system:macosx"
-      libdirs "../vendor/SFML/build-gmake/lib"
-      links {"sfml-graphics", "sfml-window", "sfml-system", "OpenGL.framework"}
+links {
+   "profile-tools",
+   "ini-parser",
+   "rk-integrator",
+   "shapes-2D",
+   "poly-physx",
+   "imgui",
+   "imgui-SFML",
+   "implot",
+   "poly-physx-app",
+   "sfml-primitives"
+}
 
-   filter {"system:windows", "configurations:debug*", "action:vs*"}
-      libdirs "../vendor/SFML/build-vs/lib/Debug"
-      links {"sfml-graphics-s-d", "sfml-window-s-d", "sfml-system-s-d"}
+filter "system:macosx"
+libdirs "../vendor/SFML/build-gmake/lib"
+links {
+   "sfml-graphics",
+   "sfml-window",
+   "sfml-system",
+   "OpenGL.framework"
+}
 
-   filter {"system:windows", "configurations:release*", "action:vs*"}
-      libdirs "../vendor/SFML/build-vs/lib/Release"
-      links {"sfml-graphics-s", "sfml-window-s", "sfml-system-s"}
+filter {
+   "system:windows",
+   "configurations:debug*",
+   "action:vs*"
+}
+libdirs "../vendor/SFML/build-vs/lib/Debug"
+links {
+   "sfml-graphics-s-d",
+   "sfml-window-s-d",
+   "sfml-system-s-d"
+}
 
-   filter {"system:windows", "action:gmake*"}
-      libdirs "../vendor/SFML/build-gmake/lib"
-      links {"sfml-graphics-s", "sfml-window-s", "sfml-system-s"}
+filter {
+   "system:windows",
+   "configurations:release*",
+   "action:vs*"
+}
+libdirs "../vendor/SFML/build-vs/lib/Release"
+links {
+   "sfml-graphics-s",
+   "sfml-window-s",
+   "sfml-system-s"
+}
 
-   filter {"system:windows", "platforms:x86_64", "action:vs*"}
-      libdirs "../vendor/SFML/extlibs/libs-msvc/x64"
-      
-   filter {"system:windows", "platforms:x86", "action:vs*"}
-      libdirs "../vendor/SFML/extlibs/libs-msvc/x86"
+filter {
+   "system:windows",
+   "action:gmake*"
+}
+libdirs "../vendor/SFML/build-gmake/lib"
+links {
+   "sfml-graphics-s",
+   "sfml-window-s",
+   "sfml-system-s"
+}
 
-   filter {"system:windows", "platforms:x86_64", "action:gmake*"}
-      libdirs "../vendor/SFML/extlibs/libs-mingw/x64"
-      
-   filter {"system:windows", "platforms:x86", "action:gmake*"}
-      libdirs "../vendor/SFML/extlibs/libs-mingw/x86"
+filter {
+   "system:windows",
+   "platforms:x86_64",
+   "action:vs*"
+}
+libdirs "../vendor/SFML/extlibs/libs-msvc/x64"
 
-   filter "system:windows"
-      links {"opengl32", "freetype", "winmm", "gdi32"}
+filter {
+   "system:windows",
+   "platforms:x86",
+   "action:vs*"
+}
+libdirs "../vendor/SFML/extlibs/libs-msvc/x86"
+
+filter {
+   "system:windows",
+   "platforms:x86_64",
+   "action:gmake*"
+}
+libdirs "../vendor/SFML/extlibs/libs-mingw/x64"
+
+filter {
+   "system:windows",
+   "platforms:x86",
+   "action:gmake*"
+}
+libdirs "../vendor/SFML/extlibs/libs-mingw/x86"
+
+filter "system:windows"
+links {
+   "opengl32",
+   "freetype",
+   "winmm",
+   "gdi32"
+}
--- premake5.lua
+++ premake5.lua
@@ -1,50 +1,61 @@
 workspace "poly-physx-demo"
-   configurations {"release", "debug", "release-profile", "debug-profile"}
-   startproject "poly-physx-demo"
+configurations {
+   "release",
+   "debug",
+   "release-profile",
+   "debug-profile"
+}
+startproject "poly-physx-demo"
 
-   function script_path()
-      local str = debug.getinfo(2, "S").source:sub(2)
-      return str:match("(.*/)")
-   end
+function script_path()
+   local str = debug.getinfo(2, "S").source:sub(2)
+   return str:match("(.*/)")
+end
 
-   rootpath = script_path()
-   
-   filter "system:macosx"
-      rpath = "-Wl,-rpath," .. rootpath .. "vendor/SFML/build-gmake/lib"
-      linkoptions {rpath}
+rootpath = script_path()
 
-      platforms {"arm64", "x86_64"}
-      
-      filter "platforms:arm64"
-         architecture "ARM64"
-      filter "platforms:x86_64"
-         architecture "x86_64"
-   filter {}
+filter "system:macosx"
+rpath = "-Wl,-rpath," .. rootpath .. "vendor/SFML/build-gmake/lib"
+linkoptions {rpath}
 
-   filter "system:windows"
-      platforms {"x86_64", "x86"}
+platforms {
+   "arm64",
+   "x86_64"
+}
 
-      filter "platforms:x86_64"
-         architecture "x86_64"
-      filter "platforms:x86"
-         architecture "x86"
-      filter {}
-   
+filter "platforms:arm64"
+architecture "ARM64"
+filter "platforms:x86_64"
+architecture "x86_64"
+filter {}
+
+filter "system:windows"
+platforms {
+   "x86_64",
+   "x86"
+}
+
+filter "platforms:x86_64"
+architecture "x86_64"
+filter "platforms:x86"
+architecture "x86"
+filter {}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}-" .. _ACTION
 defines "HAS_IMPLOT"
 
-   filter "configurations:debug*"
-      defines "DEBUG"
-      runtime "Debug"
-      symbols "On"
+filter "configurations:debug*"
+defines "DEBUG"
+runtime "Debug"
+symbols "On"
 
-   filter "configurations:release*"
-      defines "NDEBUG"
-      runtime "Release"
-      optimize "On"
+filter "configurations:release*"
+defines "NDEBUG"
+runtime "Release"
+optimize "On"
 
-   filter "configurations:*profile"
-      defines "PERF"
+filter "configurations:*profile"
+defines "PERF"
 
 include "rk-integrator"
 include "shapes-2D"
