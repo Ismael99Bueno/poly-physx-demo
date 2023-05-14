3c027fbf36e8e304c26cc681f4017315abc95a92
Ismael Bueno <ismaelwno@gmail.com>
2023-05-14 02:00:36 +0200

    Preallocating the stack at the beginning of main

--- poly-physx-demo/src/main.cpp
+++ poly-physx-demo/src/main.cpp
@@ -4,9 +4,11 @@
 
 int main()
 {
+    mem::preallocate_stack();
     DBG_SET_LEVEL(debug)
     PERF_SET_PATH("profile-results/")
     PERF_BEGIN_SESSION(PERF_SESSION_NAME, perf::profiler::HIERARCHY)
     ppx_demo::demo_app::get().run();
     PERF_END_SESSION()
+    mem::free_stack();
 }
