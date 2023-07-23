2b6c9670fe3f8bcc8befb484dde6fe8272484a79
Ismael Bueno <ismaelwno@gmail.com>
2023-07-23 22:38:21 +0100

    Medium overhaul to PPX API. Removed old demo source files for re-write

--- poly-physx-demo/premake5.lua
+++ poly-physx-demo/premake5.lua
@@ -15,12 +15,8 @@ filter "system:macosx"
       "-Wno-unused-parameter"
    }
 
-   rpath = "-Wl,-rpath,".. rootpath .."vendor/SFML/build-gmake/lib"
+   rpath = "-Wl,-rpath,".. rootpath .."vendor/vulkan-sdk/macOS/lib"
    linkoptions {rpath}
-
-filter "system:windows"
-   defines "SFML_STATIC"
-filter {}
 
 pchheader "ppxdpch.hpp"
 pchsource "src/pch.cpp"
@@ -40,107 +36,28 @@ includedirs {
    "%{wks.location}/lynx/include",
    "%{wks.location}/shapes-2D/include",
    "%{wks.location}/rk-integrator/include",
-   "%{wks.location}/sfml-primitives/include",
    "%{wks.location}/cpp-kit/include",
    "%{wks.location}/vendor/glm",
    "%{wks.location}/vendor/imgui",
-   "%{wks.location}/vendor/imgui-sfml/include",
    "%{wks.location}/vendor/implot/include",
    "%{wks.location}/vendor/yaml-cpp/include",
-   "%{wks.location}/vendor/SFML/include",
    "%{wks.location}/vendor/spdlog/include"
 }
 
 links {
+   "glfw",
+   "Cocoa.framework",
+   "IOKit.framework",
+   "CoreFoundation.framework",
+   "vulkan",
    "profile-tools",
    "yaml-cpp",
    "rk-integrator",
    "shapes-2D",
    "poly-physx",
    "imgui",
-   "imgui-SFML",
    "implot",
    "poly-physx-app",
    "lynx",
-   "sfml-primitives"
 }
 
-filter "system:macosx"
-   libdirs "%{wks.location}/vendor/SFML/build-gmake/lib"
-   links {
-      "sfml-graphics",
-      "sfml-window",
-      "sfml-system",
-      "OpenGL.framework"
-   }
-
-filter {
-   "system:windows",
-   "configurations:debug*",
-   "action:vs*"
-}
-   libdirs "%{wks.location}/vendor/SFML/build-vs/lib/Debug"
-   links {
-      "sfml-graphics-s-d",
-      "sfml-window-s-d",
-      "sfml-system-s-d"
-   }
-
-filter {
-   "system:windows",
-   "configurations:release*",
-   "action:vs*"
-}
-   libdirs "%{wks.location}/vendor/SFML/build-vs/lib/Release"
-   links {
-      "sfml-graphics-s",
-      "sfml-window-s",
-      "sfml-system-s"
-   }
-
-filter {
-   "system:windows",
-   "action:gmake*"
-}
-   libdirs "%{wks.location}/vendor/SFML/build-gmake/lib"
-   links {
-      "sfml-graphics-s",
-      "sfml-window-s",
-      "sfml-system-s"
-   }
-
-filter {
-   "system:windows",
-   "platforms:x86_64",
-   "action:vs*"
-}
-   libdirs "%{wks.location}/vendor/SFML/extlibs/libs-msvc/x64"
-
-filter {
-   "system:windows",
-   "platforms:x86",
-   "action:vs*"
-}
-   libdirs "%{wks.location}/vendor/SFML/extlibs/libs-msvc/x86"
-
-filter {
-   "system:windows",
-   "platforms:x86_64",
-   "action:gmake*"
-}
-   libdirs "%{wks.location}/vendor/SFML/extlibs/libs-mingw/x64"
-
-filter {
-   "system:windows",
-   "platforms:x86",
-   "action:gmake*"
-}
-   libdirs "%{wks.location}/vendor/SFML/extlibs/libs-mingw/x86"
-
-filter "system:windows"
-links {
-   "opengl32",
-   "freetype",
-   "winmm",
-   "gdi32"
-}
--- poly-physx-demo/src/pch.cpp
+++
@@ -1,1 +0,0 @@
-#include "ppxdpch.hpp"
