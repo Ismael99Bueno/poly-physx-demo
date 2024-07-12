4d7d2ad56d6262a477ae8822e53f857c35ff1c81
Ismael Bueno <ismaelwno@gmail.com>
2024-07-12 01:37:32 +0100

    Reduce digits

--- poly-physx-demo/src/scenarios/tumbler.cpp
+++ poly-physx-demo/src/scenarios/tumbler.cpp
@@ -99,7 +99,7 @@ void tumbler::on_imgui_window_render()
         const float time_remaining =
             (m_total_spawns - m_body_count) * m_addition_wait_time / (m_spawn_points * m_tumblers);
         if (time_remaining < 180.f)
-            ImGui::Text("Tumbler scenario progress (%.2f seconds)", time_remaining);
+            ImGui::Text("Tumbler scenario progress (%.1f seconds)", time_remaining);
         else
             ImGui::Text("Tumbler scenario progress (%.2f minutes)", time_remaining / 60.f);
         ImGui::ProgressBar((float)m_body_count / m_total_spawns, ImVec2(0.f, 0.f));
